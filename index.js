a5aec1275600a632af9a3da6312123091801d6fa
dieDeMiguel <diedemiguel@gmail.com>
2021-01-17 21:37:02 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs   = require( 'fs' );
+const http = require( 'http' );
+
+var port = process.env.PORT || 3000;
+const StaticFolder = "./public"; // "./public"
+const indexFile = '/index.html';
+
+const server = http.createServer( ( req, res )=>{
+
+  
+  req.url == '/'
+  ? req.url = indexFile 
+  : req.url;
+  
+  
+  const file = `${ StaticFolder }${ req.url }`;
+
+    fs.readFile( file, ( err, data )=>{
+
+      if ( err ){
+
+        res.writeHead( 404 );
+        res.end( 'File Not Found' );
+        return;
+      
+      }
+
+      res.writeHead( 200 );
+      res.end( data );
+
+    } );
+
+} );
+
+server.listen( port, null, ()=>{
+
+  console.log( `Server started on: ${ port }` );
+
+} );
